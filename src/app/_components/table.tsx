c875a8d9f7f0d1063bef9467a7cd8bd26a807207
Jaka Daneu <jakad@me.com>
2024-04-10 05:22:15 +0200

    feat(data-table): ✨ add Table component
    for fetching and displaying data from a JSON file

---
+++ src/app/_components/table.tsx
@@ -0,0 +1,48 @@
+import { makeFacilityRows } from '@/lib/make-facility-row';
+import { DataTable } from './data-table';
+import { makeProcedureMaxAllowedWaiting } from '@/lib/make-procedure-max-allowed-waiting';
+import { allDataSchema } from '@/lib/zod-schemas/data-schemas';
+import { columns } from './columns';
+
+const BASE_URL = 'https://mitar.gitlab.io/-/cakalne-dobe/-/jobs';
+const JSON_OUT_PATH = '/artifacts/out.json';
+
+interface TableProps {
+  jsonId: string;
+}
+
+export async function Table({ jsonId }: TableProps) {
+  let fileResponse: Response | undefined;
+  let data: unknown | undefined;
+  try {
+    fileResponse = await fetch(`${BASE_URL}/${jsonId}${JSON_OUT_PATH}`);
+    if (!fileResponse.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    data = await fileResponse.json();
+  } catch (error) {
+    return <div>Failed to fetch data</div>;
+  }
+
+  const parsedData = allDataSchema.safeParse(data);
+  if (!parsedData.success) {
+    console.error(parsedData.error.errors);
+    throw new Error('Invalid data');
+  }
+
+  const allData = parsedData.data;
+  const { procedures } = allData;
+  const rows = makeFacilityRows(procedures);
+  const allowedMaxWaitingTimes = makeProcedureMaxAllowedWaiting(procedures);
+
+  return (
+    <DataTable
+      data={rows}
+      columns={columns}
+      meta={{ allowedMaxWaitingTimes }}
+      initialState={{
+        sorting: [{ id: 'code', desc: false }],
+      }}
+    />
+  );
+}
