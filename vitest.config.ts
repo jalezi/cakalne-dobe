591457e07a1651d4917fb79451a8802ccc335a7f
Jaka Daneu <jakad@me.com>
2024-04-12 15:03:26 +0200

    test: ➕ add vitest dependency

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [tsconfigPaths(), react()],
+  test: {
+    environment: 'jsdom',
+  },
+  resolve: {
+    alias: {
+      '@lib': '/src/lib',
+      '@test': '/src/test',
+    },
+  },
+});
