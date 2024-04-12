edcbf8d09d9eb565058f4c8eeea91966e59ed641
Jaka Daneu <jakad@me.com>
2024-04-12 08:08:11 +0200

    refactor: ♻️ getJson function to use Zod schema for data validation

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -1,7 +1,6 @@
 import { makeFacilityRows } from '@/lib/make-facility-row';
 import { DataTable } from './data-table';
 import { makeProcedureMaxAllowedWaiting } from '@/lib/make-procedure-max-allowed-waiting';
-import { allDataSchema } from '@/lib/zod-schemas/data-schemas';
 import { columns } from '@/app/_components/columns';
 import { getJson } from '@/utils/get-json';
 
@@ -10,16 +9,7 @@ interface TableProps {
 }
 
 export async function Table({ jsonId }: TableProps) {
-  const data = await getJson(jsonId);
-
-  const parsedData = allDataSchema.safeParse(data);
-  if (!parsedData.success) {
-    console.error(parsedData.error.errors);
-    throw new Error('Invalid data');
-  }
-
-  const allData = parsedData.data;
-  const { procedures } = allData;
+  const { procedures } = await getJson(jsonId);
   const rows = makeFacilityRows(procedures);
   const allowedMaxWaitingTimes = makeProcedureMaxAllowedWaiting(procedures);
 
--- src/utils/get-json.ts
+++ src/utils/get-json.ts
@@ -1,15 +1,23 @@
+import { allDataSchema, type AllData } from '@/lib/zod-schemas/data-schemas';
+
 const BASE_URL = new URL('https://mitar.gitlab.io');
 export const BASE_JOBS_URL = new URL('-/cakalne-dobe/-/jobs', BASE_URL);
 const JSON_OUT_PATH = '/artifacts/out.json';
 
-export const getJson = async (id: string) => {
+export const getJson = async (id: string): Promise<AllData> => {
   try {
     const fileResponse = await fetch(`${BASE_JOBS_URL}/${id}${JSON_OUT_PATH}`);
     if (!fileResponse.ok) {
       throw new Error('Failed to fetch data');
     }
     const data = await fileResponse.json();
-    return data;
+    const parsedData = allDataSchema.safeParse(data);
+    if (!parsedData.success) {
+      console.error(parsedData.error.errors);
+      throw new Error('Invalid data');
+    }
+
+    return parsedData.data;
   } catch (error) {
     console.error(error);
     throw new Error('Unknown error');
