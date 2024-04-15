f65028f858fd419ea1c84fd23326cf37dcb48d6b
Jaka Daneu <jakad@me.com>
2024-04-15 11:38:30 +0200

    feat: ✨ add label on above sm screen in JobPagination component

--- src/components/jobs-pagination.tsx
+++ src/components/jobs-pagination.tsx
@@ -4,6 +4,7 @@ import { Button } from './ui/button';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { JobLink } from './job-link';
 import { cn } from '@/lib/utils';
+import { Time } from './time';
 
 interface JobsPaginationProps {
   id: string;
@@ -27,56 +28,77 @@ export async function JobsPagination({ id }: JobsPaginationProps) {
     return null;
   }
 
-  const nextJob = jobIndex > 0 ? jobsOptions[jobIndex - 1] : null;
-  const previousJob =
+  const previousJob = jobIndex > 0 ? jobsOptions[jobIndex - 1] : null;
+  const previousJobDate = previousJob ? new Date(previousJob.label) : null;
+  const nextJob =
     jobIndex < jobsOptions.length - 1 ? jobsOptions[jobIndex + 1] : null;
+  const nextJobDate = nextJob ? new Date(nextJob.label) : null;
 
   return (
     <div className="flex">
-      <Button
-        asChild
-        variant="secondary"
-        size="icon"
-        disabled={!!nextJob}
-        className={cn(!nextJob && 'invisible')}
-        aria-label={
-          nextJob
-            ? `podatki pridobljeni: ${new Date(nextJob.label).toLocaleString()}`
-            : 'No previous job'
-        }
-      >
-        {nextJob ? (
-          <JobLink id={nextJob.value}>
-            <ChevronLeft size={16} />
-          </JobLink>
-        ) : (
-          <span>
-            <ChevronLeft size={16} />
-          </span>
-        )}
-      </Button>
-      <Button
-        asChild
-        variant="secondary"
-        size="icon"
-        disabled={!!previousJob}
-        className={cn(!previousJob && 'invisible', 'ml-auto')}
-        aria-label={
-          previousJob
-            ? `podatki pridobljeni: ${new Date(previousJob.label).toLocaleString()}`
-            : 'No previous job'
-        }
-      >
-        {previousJob ? (
-          <JobLink id={previousJob.value}>
-            <ChevronRight size={16} />
-          </JobLink>
-        ) : (
-          <span>
-            <ChevronRight size={16} />
-          </span>
-        )}
-      </Button>
+      {previousJob ? (
+        <>
+          <Button
+            asChild
+            variant="secondary"
+            size="icon"
+            disabled={!!previousJob}
+            className={cn(!previousJob && 'hidden', 'sm:hidden')}
+            aria-label={
+              previousJobDate
+                ? `podatki pridobljeni: ${previousJobDate.toLocaleDateString()}`
+                : 'neznan datum'
+            }
+          >
+            <JobLink id={previousJob.value}>
+              <ChevronLeft size={16} />
+            </JobLink>
+          </Button>
+          <Button
+            asChild
+            variant="link"
+            className={cn(!nextJob && 'hidden', 'hidden pl-0 sm:inline-flex')}
+          >
+            <JobLink id={previousJob.value}>
+              <ChevronLeft size={16} />
+              {previousJobDate ? <Time date={previousJobDate} /> : null}
+            </JobLink>
+          </Button>
+        </>
+      ) : null}
+      {nextJob ? (
+        <>
+          <Button
+            asChild
+            variant="secondary"
+            size="icon"
+            disabled={!!nextJob}
+            className={cn(!nextJob && 'hidden', 'ml-auto sm:hidden')}
+            aria-label={
+              nextJobDate
+                ? `podatki pridobljeni: ${nextJobDate.toLocaleDateString()}`
+                : 'neznan datum'
+            }
+          >
+            <JobLink id={nextJob.value}>
+              <ChevronRight size={16} />
+            </JobLink>
+          </Button>
+          <Button
+            asChild
+            variant="link"
+            className={cn(
+              !nextJob && 'hidden',
+              'ml-auto hidden pr-0 sm:inline-flex'
+            )}
+          >
+            <JobLink id={nextJob.value}>
+              {nextJobDate ? <Time date={nextJobDate} /> : null}
+              <ChevronRight size={16} />
+            </JobLink>
+          </Button>
+        </>
+      ) : null}
     </div>
   );
 }
