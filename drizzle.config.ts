1287898fbfc003f5de43164398637fe3747231d7
Jaka Daneu <jakad@me.com>
2024-04-16 21:13:01 +0200

    chore(db): 🚧 add support for drizzle-kit and eslint

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,16 @@
 import type { Config } from 'drizzle-kit';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './.env.development.local' });
+// dotenv.config({ path: './.env.production.local' });
 
 export default {
   schema: './src/db/schema/*',
   driver: 'turso',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL,
     authToken: process.env.DATABASE_AUTH_TOKEN,
   },
   out: './drizzle',
+  verbose: true,
 } satisfies Config;
