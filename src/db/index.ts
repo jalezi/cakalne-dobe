4bcf2ecda74bbf00fa3d96c58847c40c66fb0983
Jaka Daneu <jakad@me.com>
2024-04-17 17:08:00 +0200

    feat(db): 🏷️ get db query type safety

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,10 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 import { ENV_SERVER_VARS } from '@/lib/env';
+import * as schema from './schema';
 
 const client = createClient({
   url: ENV_SERVER_VARS.DATABASE_URL,
   authToken: ENV_SERVER_VARS.DATABASE_AUTH_TOKEN || undefined,
 });
-export const db = drizzle(client);
+export const db = drizzle(client, { schema });
---
+++ src/db/schema/index.ts
@@ -0,0 +1,5 @@
+export { jobs } from './jobs';
+export { institutions } from './institution';
+export { maxWaitingPeriods } from './max-waiting-periods';
+export { procedures } from './procedures';
+export { waitingPeriods } from './waiting-periods';
