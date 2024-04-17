e10f49375fb62f5e342817033c36bdd633d2431e
Jaka Daneu <jakad@me.com>
2024-04-17 17:09:49 +0200

    feat(db): 🗃️ add unique index for job and max_waiting_period

--- src/db/schema/jobs.ts
+++ src/db/schema/jobs.ts
@@ -1,21 +1,29 @@
-import { sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { sqliteTable, text, unique } from 'drizzle-orm/sqlite-core';
 import { timestamps } from '../schema-common-fields';
 import { relations } from 'drizzle-orm';
 import { maxWaitingPeriods } from './max-waiting-periods';
 import { waitingPeriods } from './waiting-periods';
 import { createId } from '@paralleldrive/cuid2';
 
-export const jobs = sqliteTable('jobs', {
-  id: text('id')
-    .primaryKey()
-    .$defaultFn(() => createId()),
-  gitLabJobId: text('git_lab_job_id')
-    .$default(() => '')
-    .notNull(),
-  startDate: text('start_date').notNull(),
-  endDate: text('end_date').notNull(),
-  ...timestamps,
-});
+export const jobs = sqliteTable(
+  'jobs',
+  {
+    id: text('id')
+      .primaryKey()
+      .$defaultFn(() => createId()),
+    gitLabJobId: text('git_lab_job_id')
+      .$default(() => '')
+      .notNull(),
+    startDate: text('start_date').notNull(),
+    endDate: text('end_date').notNull(),
+    ...timestamps,
+  },
+  (table) => {
+    return {
+      gitLabIndex: unique().on(table.gitLabJobId),
+    };
+  }
+);
 
 export const jobRelations = relations(jobs, ({ many }) => ({
   maxWaitingPeriods: many(maxWaitingPeriods, {
--- src/db/schema/max-waiting-periods.ts
+++ src/db/schema/max-waiting-periods.ts
@@ -1,25 +1,33 @@
-import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { integer, sqliteTable, text, unique } from 'drizzle-orm/sqlite-core';
 import { timestamps } from '../schema-common-fields';
 import { jobs } from './jobs';
 import { procedures } from './procedures';
 import { relations } from 'drizzle-orm';
 import { createId } from '@paralleldrive/cuid2';
 
-export const maxWaitingPeriods = sqliteTable('max_waiting_periods', {
-  id: text('id')
-    .primaryKey()
-    .$defaultFn(() => createId()),
-  regular: integer('regular').notNull(),
-  fast: integer('fast').notNull(),
-  veryFast: integer('very_fast').notNull(),
-  jobId: text('job_id')
-    .notNull()
-    .references(() => jobs.id),
-  procedureId: text('procedure_id')
-    .notNull()
-    .references(() => procedures.id),
-  ...timestamps,
-});
+export const maxWaitingPeriods = sqliteTable(
+  'max_waiting_periods',
+  {
+    id: text('id')
+      .primaryKey()
+      .$defaultFn(() => createId()),
+    regular: integer('regular').notNull(),
+    fast: integer('fast').notNull(),
+    veryFast: integer('very_fast').notNull(),
+    jobId: text('job_id')
+      .notNull()
+      .references(() => jobs.id),
+    procedureId: text('procedure_id')
+      .notNull()
+      .references(() => procedures.id),
+    ...timestamps,
+  },
+  (table) => {
+    return {
+      maxWaitingPeriodsIndex: unique().on(table.jobId, table.procedureId),
+    };
+  }
+);
 
 export const maxWaitingPeriodsRelations = relations(
   maxWaitingPeriods,
