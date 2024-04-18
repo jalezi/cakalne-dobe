9208cf6a4251a2c9548c7c7a2b822070fd193ed9
Jaka Daneu <jakad@me.com>
2024-04-18 13:16:08 +0200

    feat(zod-schemas): ✨ add normalizeFPWTSchema for facility procedure waiting times

--- src/lib/zod-schemas/helpers-schema.ts
+++ src/lib/zod-schemas/helpers-schema.ts
@@ -1,12 +1,29 @@
 import { z } from 'zod';
+import { facilityProcedureWaitingTimesSchema } from './data-schemas';
 
 export const trimmedStringSchema = z
   .string()
   .transform((s) => s.replace(/\s+/g, ' ').trim());
 
-export const normalizeProcedureNameSchema = z.string().transform((s) =>
+export const trimStringWithDashes = z.string().transform((s) =>
   s
     .split('-')
     .map((w) => trimmedStringSchema.parse(w))
     .join('-')
 );
+
+export const normalizeFPWTSchema =
+  facilityProcedureWaitingTimesSchema.transform((val) => {
+    return {
+      facility: trimmedStringSchema.parse(val.facility),
+      procedure: {
+        code: val.procedure.code,
+        name: trimStringWithDashes.parse(val.procedure.name),
+      },
+      waitingPeriods: {
+        regular: val.waitingPeriods.regular,
+        fast: val.waitingPeriods.fast,
+        veryFast: val.waitingPeriods.veryFast,
+      },
+    };
+  });
