7eb6ad6701cb8fd445f2c0f69d1e1c5692358ada
Jaka Daneu <jakad@me.com>
2024-04-22 20:28:28 +0200

    feat: ✨ Add TimeRange component to display date range in Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { DateRangeJobLinks } from '@/components/date-range-job-links';
 import { db } from '@/db';
 import { desc } from 'drizzle-orm';
 import { jobs as jobsTable } from '@/db/schema';
+import { TimeRange } from '@/components/time';
 
 export default async function Home() {
   const jobs = await db.query.jobs.findMany({
@@ -14,11 +15,25 @@ export default async function Home() {
     label: job.startDate,
   }));
 
+  const firstJob = jobs.at(-1);
+  const lastJob = jobs.at(0);
+
   return (
     <main className="z-0 space-y-2 p-4">
       <h1 id="attr-h1" className="text-2xl font-bold" aria-labelledby="attr-h1">
         Čakalne dobe
       </h1>
+      <div>
+        Podatki zbrani za obdobje:{' '}
+        {firstJob && lastJob ? (
+          <TimeRange
+            startDate={firstJob.startDate}
+            endDate={lastJob.startDate}
+            options={{ timeZone: 'Europe/Ljubljana' }}
+          />
+        ) : null}
+      </div>
+      <h2 className="text-xl font-bold">Povezave do čakalnih dob</h2>
       <DateRangeJobLinks links={jobsOptions} />
     </main>
   );
