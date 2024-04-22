3b164166ac751a9b4bbfc9f9d8f5983c9a460998
Jaka Daneu <jakad@me.com>
2024-04-22 20:28:28 +0200

    fix: ♿️ missing label for select

--- src/components/date-range-job-links.tsx
+++ src/components/date-range-job-links.tsx
@@ -23,6 +23,7 @@ import {
 } from '@/components/ui/select';
 import Link from 'next/link';
 import { Time, TimeRange } from './time';
+import { Label } from './ui/label';
 
 // day @mitar has started to collect data for the first time
 const FIRST_DAY = new Date(2024, 3, 7);
@@ -69,79 +70,85 @@ export function DateRangeJobLinks({
 
   return (
     <>
-      <Popover>
-        <PopoverTrigger asChild>
-          <Button
-            variant={'outline'}
-            className={cn(
-              'w-[280px] justify-start text-left font-normal',
-              !date && 'text-muted-foreground'
-            )}
-          >
-            <CalendarIcon className="mr-2 h-4 w-4" />
-            {date?.from ? (
-              date.to ? (
-                <TimeRange
-                  startDate={date.from}
-                  endDate={date.to}
-                  locale="sl"
-                />
+      <div className="flex flex-col gap-2">
+        <Label htmlFor="date-range">Časovno obdobje</Label>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              variant={'outline'}
+              className={cn(
+                'w-[280px] justify-start text-left font-normal',
+                !date && 'text-muted-foreground'
+              )}
+            >
+              <CalendarIcon className="mr-2 h-4 w-4" />
+              {date?.from ? (
+                date.to ? (
+                  <TimeRange
+                    startDate={date.from}
+                    endDate={date.to}
+                    locale="sl"
+                  />
+                ) : (
+                  <Time date={date.from} />
+                )
               ) : (
-                <Time date={date.from} />
-              )
-            ) : (
-              <span>Izberi časovno obdobje</span>
-            )}
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="flex w-auto flex-col space-y-2 p-2">
-          <Select
-            onValueChange={(value) => {
-              const today = new Date();
-              switch (value) {
-                case '0':
-                  setDate({
-                    from: new Date(
-                      new Date().setDate(
-                        today.getDate() - (today.getDay() || 7) + 1
-                      )
-                    ),
-                    to: today,
-                  });
+                <span>Izberi časovno obdobje</span>
+              )}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="flex w-auto flex-col space-y-2 p-2">
+            <Select
+              onValueChange={(value) => {
+                const today = new Date();
+                switch (value) {
+                  case '0':
+                    setDate({
+                      from: new Date(
+                        new Date().setDate(
+                          today.getDate() - (today.getDay() || 7) + 1
+                        )
+                      ),
+                      to: today,
+                    });
 
-                  break;
-                case '1':
-                  setDate({
-                    from: addDays(today, -6),
-                    to: today,
-                  });
-                  break;
-              }
-            }}
-          >
-            <SelectTrigger>
-              <SelectValue placeholder="Izberi časovno obdobje" />
-            </SelectTrigger>
-            <SelectContent position="popper">
-              <SelectItem value="0">Ta teden</SelectItem>
-              <SelectItem value="1">Zadnjih 7 dni</SelectItem>
-            </SelectContent>
-          </Select>
-          <div className="rounded-md border">
-            <Calendar
-              initialFocus
-              mode="range"
-              selected={date}
-              onSelect={setDate}
-              numberOfMonths={2}
-              disabled={(day) => day > new Date() || day < new Date(2024, 3, 7)}
-              locale={sl}
-              fromDate={FIRST_DAY}
-              toDate={new Date()}
-            />
-          </div>
-        </PopoverContent>
-      </Popover>
+                    break;
+                  case '1':
+                    setDate({
+                      from: addDays(today, -6),
+                      to: today,
+                    });
+                    break;
+                }
+              }}
+            >
+              <SelectTrigger id="date-range">
+                <SelectValue placeholder="Izberi časovno obdobje" />
+              </SelectTrigger>
+              <SelectContent position="popper">
+                <SelectItem value="0">Ta teden</SelectItem>
+                <SelectItem value="1">Zadnjih 7 dni</SelectItem>
+              </SelectContent>
+            </Select>
+            <div className="rounded-md border">
+              <Calendar
+                initialFocus
+                mode="range"
+                selected={date}
+                onSelect={setDate}
+                numberOfMonths={2}
+                disabled={(day) =>
+                  day > new Date() || day < new Date(2024, 3, 7)
+                }
+                locale={sl}
+                fromDate={FIRST_DAY}
+                toDate={new Date()}
+              />
+            </div>
+          </PopoverContent>
+        </Popover>
+      </div>
+
       <nav className="px-4 py-2">
         <ul>
           {filteredLinks.map((job) => (
