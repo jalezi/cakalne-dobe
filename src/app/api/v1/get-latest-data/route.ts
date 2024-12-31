b9e5cfc09a3162c6be866eae078bfd6db010f649
Jaka Daneu <jakad@me.com>
2024-12-31 05:14:37 +0100

    refactor: 🚧 update BASE_URL to new repository location

--- src/app/api/v1/get-latest-data/route.ts
+++ src/app/api/v1/get-latest-data/route.ts
@@ -2,7 +2,7 @@ import { handleError } from '@/utils/handle-error';
 import type { AllData } from '@/lib/zod-schemas/data-schemas';
 import { getLastJobId } from '@/utils/get-last-job-id';
 
-const BASE_URL = new URL('https://mitar.gitlab.io');
+const BASE_URL = new URL('https://wayback-automachine.gitlab.io');
 const BASE_JOBS_URL = new URL('-/cakalne-dobe/-/jobs', BASE_URL);
 const JSON_OUT_PATH = '/artifacts/out.json';
 
