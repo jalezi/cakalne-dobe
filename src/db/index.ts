b81fecad8b0d4910f717e8a6427349dd0b4fcc4e
Jaka Daneu <jakad@me.com>
2025-03-22 15:14:41 +0100

    chore: remove debug console logs from environment variable files

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,11 +3,6 @@ import { createClient } from '@libsql/client';
 import { ENV_SERVER_VARS } from '@/lib/env';
 import * as schema from './schema';
 
-console.log("hello from src/db/index.ts");
-
-console.log('ENV_SERVER_VARS.DATABASE_URL', ENV_SERVER_VARS.DATABASE_URL);
-console.log('ENV_SERVER_VARS.DATABASE_AUTH_TOKEN', ENV_SERVER_VARS.DATABASE_AUTH_TOKEN);
-
 const client = createClient({
   url: ENV_SERVER_VARS.DATABASE_URL,
   authToken: ENV_SERVER_VARS.DATABASE_AUTH_TOKEN || undefined,
