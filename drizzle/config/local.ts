62b5fb9f6e9310ddee407d65add9a30b560ccea4
Jaka Daneu <jakad@me.com>
2025-03-29 11:20:03 +0100

    chore: update database credentials to use authToken and change dialect to turso

--- drizzle/config/local.ts
+++ drizzle/config/local.ts
@@ -7,9 +7,9 @@ export default defineConfig({
   schema: './src/db/schema/*',
   dbCredentials: {
     url: process.env.DATABASE_URL,
-    accountId: process.env.DATABASE_AUTH_TOKEN || undefined,
+    authToken: process.env.DATABASE_AUTH_TOKEN || undefined,
   },
   out: './drizzle',
   verbose: true,
-  dialect: 'sqlite',
+  dialect: 'turso',
 });
--- drizzle/config/prod.ts
+++ drizzle/config/prod.ts
@@ -2,15 +2,17 @@ import { defineConfig } from 'drizzle-kit';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: './.env.production.local' });
+console.log(process.env.DATABASE_URL);
+console.log(process.env.DATABASE_AUTH_TOKEN);
 
 export default defineConfig({
   schema: './src/db/schema/*',
 
   dbCredentials: {
     url: process.env.DATABASE_URL,
-    accountId: process.env.DATABASE_AUTH_TOKEN || undefined,
+    authToken: process.env.DATABASE_AUTH_TOKEN || undefined,
   },
   out: './drizzle',
   verbose: true,
-  dialect: 'sqlite',
+  dialect: 'turso',
 });
